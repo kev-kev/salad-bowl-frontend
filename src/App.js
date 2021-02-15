3d56bef97684b6f3cdda75316adf86c1ca33cd35
kev-kev <kevin.clark93@gmail.com>
2021-02-15 16:03:19 -0500

    Setting up front page of app with buttons for create/join room

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
 import React, { useState, useEffect } from "react";
 import socketIOClient from "socket.io-client";
-import "./index.css";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import saladBowlSVG from "./assets/images/saladbowl.svg";
+
 const serverUrl = "http://localhost:4001/";
 
 function App() {
-  const [response, setResponse] = useState("");
+  const [setResponse] = useState("");
 
   useEffect(() => {
     const socket = socketIOClient(serverUrl);
@@ -14,23 +19,24 @@ function App() {
   });
 
   return (
-    <div>
-      <head>
-        <title>Socket.IO Chat</title>
-      </head>
-      <body>
-        <form id="usernameForm">
-          <label>Enter a Username</label>
-          <br />
-          <input id="usernameInput" autocomplete="off" />
-          <button>Submit</button>
-        </form>
-        <ul id="messages"></ul>
-        <form id="messageForm">
-          <input id="messageInput" autocomplete="off" />
-          <button>Send</button>
-        </form>
-      </body>
+    <div className="d-flex flex-column min-vh-100 justify-content-center align-items-center">
+      <img src={saladBowlSVG} className="home-logo" />
+      <h3 className="mb-5">Salad Bowl</h3>
+      <Button className="home-button mb-4" variant="outline-primary" size="lg">
+        Create Room
+      </Button>
+      <span className="mb-4">- or -</span>
+      <Form className="d-flex">
+        <Form.Control
+          className="mr-2"
+          size="lg"
+          type="text"
+          placeholder="Enter Room Code"
+        />
+        <Button className="home-button mb-0" variant="outline-secondary">
+          Join Room
+        </Button>
+      </Form>
     </div>
   );
 }
