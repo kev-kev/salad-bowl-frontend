6b28a1a30cc4bb3543ec67fb7dad4790e1624d42
kev-kev <kevin.clark93@gmail.com>
2021-06-22 17:43:44 -0400

    moving createRoomCode to backend, removing junk from testing, emitting join room in the callback for create room

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -7,35 +7,14 @@ import saladBowlSVG from "../assets/images/saladbowl.svg";
 const serverUrl = "http://localhost:4001/";
 const socket = socketIOClient(serverUrl);
 
-function createRoomCode() {
-  var result = "";
-  var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var charactersLength = characters.length;
-  for (var i = 0; i < 5; i++) {
-    result += characters.charAt(Math.floor(Math.random() * charactersLength));
-  }
-  return result;
-}
-
 function Homepage(props) {
   const [enteredRoomCode, setEnteredRoomCode] = useState("");
   const [inRoom, setInRoom] = useState(false);
-  const [shouldShowLogo, setShouldShowLogo] = useState(true);
-  // const { setRoomCode, socket, setSocket } = useContext(GlobalContext);
-
-  useEffect(() => {
-    socket.on("receive toggle logo", () => {
-      console.log("toggled in frontend");
-      setShouldShowLogo(false);
-    });
-  }, []);
 
   const createNewRoom = () => {
-    const roomCode = createRoomCode();
-    // setRoomCode(roomCode);
-    socket.emit("create room", roomCode);
-    socket.emit("join room", roomCode);
-    setEnteredRoomCode(roomCode);
+    socket.emit("create room", (response) => {
+      socket.emit("join room", response.code);
+    });
   };
 
   const joinRoom = (code) => {
@@ -43,18 +22,12 @@ function Homepage(props) {
       console.log("Invalid room code");
     } else {
       socket.emit("join room", code);
-      setEnteredRoomCode(code);
     }
-  };
-
-  const handleTest = (enteredRoomCode) => {
-    console.log("entered room code: ", enteredRoomCode);
-    socket.emit("toggle logo", enteredRoomCode);
   };
 
   return (
     <div className="d-flex flex-column min-vh-100 justify-content-center align-items-center">
-      {shouldShowLogo && <img src={saladBowlSVG} className="home-logo" />}
+      <img src={saladBowlSVG} className="home-logo" />
       <h3 className="mb-5">Salad Bowl</h3>
       <Button
         className="home-button mb-4"
@@ -84,13 +57,6 @@ function Homepage(props) {
         </Button>
       </Form>
       <br />
-      <Button
-        className="home-button mb-0"
-        variant="outline-danger"
-        onClick={() => handleTest(enteredRoomCode)}
-      >
-        Test
-      </Button>
     </div>
   );
 }
