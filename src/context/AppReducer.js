192e0419458f24a549dd772bfc2963620d0d6945
kev-kev <kevin.clark93@gmail.com>
2021-07-19 18:18:59 -0400

    putting everything in global context, about to transfer stuff to component state

--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -1,9 +1,10 @@
 export default (state, action) => {
   switch (action.type) {
     case "SET_ROOM":
+      // action.payload = {team1: [], team2: [], deck: [], discard: [], cluegiver: "user1"}
       return {
         ...state,
-        room: action.payload,
+        deck: action.payload.deck,
       };
     case "SET_USERNAME":
       return {
@@ -15,27 +16,42 @@ export default (state, action) => {
         ...state,
         phase: action.payload,
       };
-    case "CLEAR_STATE":
+    case "UPDATE_TEAM1":
       return {
-        ...action.payload,
+        ...state,
+        team1: action.payload,
       };
-    case "ADD_USER_TO_TEAM":
-      console.log(action.payload);
-      if (action.payload.teamIndex === 0) {
-        return {
-          ...state,
-          team1: [...state.team1, action.payload.username],
-        };
-      } else if (action.payload.teamIndex === 1) {
-        return {
-          ...state,
-          team2: [...state.team2, action.payload.username],
-        };
-      }
+    case "UPDATE_TEAM2":
+      return {
+        ...state,
+        team2: action.payload,
+      };
+    case "SET_ROOM_OWNER":
+      return {
+        ...state,
+        roomOwner: action.payload,
+      };
+    // case "ADD_USER_TO_TEAM":
+    //   console.log(action.payload);
+    //   if (action.payload.teamIndex === 0) {
+    //     return {
+    //       ...state,
+    //       team1: [...state.team1, action.payload.username],
+    //     };
+    //   } else if (action.payload.teamIndex === 1) {
+    //     return {
+    //       ...state,
+    //       team2: [...state.team2, action.payload.username],
+    //     };
+    //   }
     case "SET_ERROR":
       return {
         ...state,
         error: action.payload,
+      };
+    case "CLEAR_STATE":
+      return {
+        ...action.payload,
       };
     default:
       return state;
--- src/context/GlobalContext.js
+++ src/context/GlobalContext.js
@@ -34,6 +34,55 @@ export const GlobalProvider = ({ children }) => {
     });
   }
 
+  function setRoomCode(roomCode) {
+    dispatch({
+      type: "SET_ROOMCODE",
+      payload: roomCode,
+    });
+  }
+
+  function updateTeam(teamArr, teamIndex) {
+    dispatch({
+      type: `UPDATE_TEAM${teamIndex + 1}`,
+      payload: teamArr,
+    });
+  }
+
+  function setPhase(phase) {
+    dispatch({
+      type: "SET_PHASE",
+      payload: phase,
+    });
+  }
+
+  function setRoomOwner(username) {
+    dispatch({
+      type: "SET_ROOM_OWNER",
+      payload: username,
+    });
+  }
+
+  function updateDeck(deck) {
+    dispatch({
+      type: "UPDATE_DECK",
+      payload: deck,
+    });
+  }
+
+  function updateDiscard(discard) {
+    dispatch({
+      type: "UPDATE_DISCARD",
+      payload: discard,
+    });
+  }
+
+  function setClueGiver(username) {
+    dispatch({
+      type: "SET_CLUE_GIVER",
+      payload: username,
+    });
+  }
+
   function clearState(initialState) {
     dispatch({
       type: "CLEAR_STATE",
@@ -48,27 +97,40 @@ export const GlobalProvider = ({ children }) => {
     });
   }
 
-  function addUserToTeam(username, teamIndex) {
-    console.log("adding user to team");
-    dispatch({
-      type: "ADD_USER_TO_TEAM",
-      payload: { username, teamIndex },
-    });
-  }
+  // function addUserToTeam(username, teamIndex) {
+  //   console.log("adding user to team");
+  //   dispatch({
+  //     type: "ADD_USER_TO_TEAM",
+  //     payload: { username, teamIndex },
+  //   });
+  // }
 
   return (
     <GlobalContext.Provider
       value={{
         username: state.username,
         setUsername,
-        room: state.room,
-        error: state.error,
-        updateRoom,
-        clearState,
-        setError,
-        addUserToTeam,
         team1: state.team1,
         team2: state.team2,
+        updateTeam,
+        roomCode: state.roomCode,
+        setRoomCode,
+        phase: state.phase,
+        setPhase,
+        roomOwner: state.roomOwner,
+        setRoomOwner,
+        deck: state.deck,
+        updateDeck,
+        discard: state.discard,
+        updateDiscard,
+        clueGiver: state.clueGiver,
+        setClueGiver,
+        clearState,
+        error: state.error,
+        setError,
+        // room: state.room,
+        // updateRoom,
+        // addUserToTeam,
       }}
     >
       {children}
